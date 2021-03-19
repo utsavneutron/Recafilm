79e3ae1a57bb33fe8f1394715b3fb16e5b10c83e
Utsav Shrestha <54687372+utsavneutron@users.noreply.github.com>
2021-03-18 22:12:39 -0500

    Created Recommendation API and uploaded to Heroku

---
+++ Recommendation API for movies/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn app:app --max-requests 2
---
+++ Recommendation API for movies/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import recommendation
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/movie', methods=['GET'])
+def recommend_movies():
+    res = recommendation.results(request.args.get('title'))
+    return jsonify(res)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
