71c128a5d4360c9c20c58f46f748ad9f067e9bad
Utsav Shrestha <utsav.shrestha@mavs.uta.edu>
2021-04-26 22:39:59 -0500

    Created database based on Django

--- js/logout.js
+++
@@ -1,9 +0,0 @@
-const auth = firebase.auth();
-const logout = document.querySelector('#logout');
-
-logout.addEventListener('click', (e)=>{
-    
-    e.preventDefault();
-    auth.signOut()
-    window.location = '../index.html'
-});
