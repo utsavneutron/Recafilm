30e660e547e4eeca995e99a5bb39b93f8edbc0d1
Utsav Shrestha <utsav.shrestha@mavs.uta.edu>
2021-04-27 22:52:59 -0500

    Uploaded to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn recafilm.wsgi --log-file -
--- recafilm/settings.py
+++ recafilm/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recaafilm'
+
 ]
 
 MIDDLEWARE = [
@@ -75,13 +76,22 @@ WSGI_APPLICATION = 'recafilm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'recafilm',
+#         'USER': 'utsavshrestha',
+#         'PASSWORD': 'recafilm123',
+#         'HOST': 'database-1.cxonyoqdkurj.us-east-2.rds.amazonaws.com',
+#         'PORT': '5432'
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
